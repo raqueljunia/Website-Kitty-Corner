6f812c56701b8a9a76a02358f9d040af0fe9f2cc
yohanandre <yohanandrey14@gmail.com>
2023-05-17 02:51:07 +0700

    Menu

--- src/pages/CatHotel.js
+++ src/pages/CatHotel.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import catHotel1 from '../assets/img/catHotel1.jpg';
+import catHotel2 from '../assets/img/catHotel2.jpg';
+import priceListImage from '../assets/img/PriceListHotel.jpg';
+import '../index.css';
 
 const CatHotel = () => {
   return (
-    <div>
-      <h1>Cat Motel</h1>
-      <p>Cat Hotel Page.</p>
-
+    <div className="cat-hotel-container">
+      <div className="cat-hotel-image">
+        <img src={catHotel1} alt="Cat Hotel" />
+        <div className="text-container">
+          <h1 className="cat-hotel-title">Cat Hotel by Kitty Corner</h1>
+        </div>
+      </div>
+      <div className="additional-content">
+        <div className="text-content">
+          <h2 className="welcome-text">Welcome to Pet Hotel by Kitty Corner!</h2>
+          <p className="description-text">
+            We are a high-quality pet hotel that offers comfortable and safe care for your beloved cats.
+          </p>
+        </div>
+        <div className="additional-image">
+          <img src={catHotel2} alt="Additional Cat Hotel" />
+        </div>
+      </div>
+      <div className="price-list">
+        <h2 className="price-list-title">Price List</h2>
+        <img src={priceListImage} alt="Price List Hotel" className="price-list-image" />
+      </div>
     </div>
   );
 }
