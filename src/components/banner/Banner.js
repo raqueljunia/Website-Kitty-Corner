661d105393b2fbcb925804a067cabcb4f8155d16
rifqahaulia <129581643+rifqahaulia@users.noreply.github.com>
2023-05-18 23:32:02 +0700

    banner

---
+++ src/components/banner/Banner.js
@@ -0,0 +1,13 @@
+import react from 'react'
+import './Banner.css'
+
+const Banner = () => {
+    return (
+        <div className='banner'>
+            <h1 className='banner-title'>KITTY CORNER CAT SALON</h1>
+            <div className='kitties-img'></div>
+        </div>
+    )
+}
+
+export default Banner;
