37c7121f0bdd86912dd4b892117f0a8f5e032922
Faizul <faizul200429@gmail.com>
2023-05-19 13:23:35 +0700

    caffe product

--- src/pages/Cafe.js
+++ src/pages/Cafe.js
@@ -2,10 +2,163 @@ import React from 'react';
 
 const Cafe = () => {
   return (
-    <div>
-      <h1>Cafe</h1>
-      <p>Cafe page.</p>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6 col-md-3 ">
+                <div class="card-product card-product--menu-barry-shake">
+                    <div class="overlay">
+                        <div class = "items"></div>
+                        <div class = "items head">
+                            <p>BARRY SHAKE</p>
+                            <hr></hr>
+                        </div>
+                        <div class = "items price">
+                            <p class="old">Rp20.000</p>
+                            <p class="new">Rp14.000</p>
+                        </div>
+                        <div class="items cart">
+                            <i class="fa fa-shopping-cart"></i>
+                            <span>BUY NOW</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3 ">
+                <div class="card-product card-product--menu-caramel-shake">
+                    <div class="overlay">
+                        <div class = "items"></div>
+                        <div class = "items head">
+                            <p>CARAMEL SHAKE</p>
+                            <hr></hr>
+                        </div>
+                        <div class = "items price">
+                            <p class="old">Rp26.000</p>
+                            <p class="new">Rp17.000</p>
+                        </div>
+                        <div class="items cart">
+                            <i class="fa fa-shopping-cart"></i>
+                            <span>BUY NOW</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3 ">
+                <div class="card-product card-product--menu-oreo-shake">
+                    <div class="overlay">
+                        <div class = "items"></div>
+                        <div class = "items head">
+                            <p>OREO SHAKE</p>
+                            <hr></hr>
+                        </div>
+                        <div class = "items price">
+                            <p class="old">Rp22.000</p>
+                            <p class="new">Rp18.000</p>
+                        </div>
+                        <div class="items cart">
+                            <i class="fa fa-shopping-cart"></i>
+                            <span>BUY NOW</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3 ">
+                <div class="card-product card-product--menu-macha-shake">
+                    <div class="overlay">
+                        <div class = "items"></div>
+                        <div class = "items head">
+                            <p>MATCHA SHAKE</p>
+                            <hr></hr>
+                        </div>
+                        <div class = "items price">
+                            <p class="old">Rp28.000</p>
+                            <p class="new">Rp29.000</p>
+                        </div>
+                        <div class="items cart">
+                            <i class="fa fa-shopping-cart"></i>
+                            <span>BUY NOW</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3 ">
+                <div class="card-product card-product--menu-kwetiaw">
+                    <div class="overlay">
+                        <div class = "items"></div>
+                        <div class = "items head">
+                            <p>KWETIAW</p>
+                            <hr></hr>
+                        </div>
+                        <div class = "items price">
+                            <p class="old">Rp25.000</p>
+                            <p class="new">Rp18.000</p>
+                        </div>
+                        <div class="items cart">
+                            <i class="fa fa-shopping-cart"></i>
+                            <span>BUY NOW</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3 ">
+                <div class="card-product card-product--menu-omlet">
+                    <div class="overlay">
+                        <div class = "items"></div>
+                        <div class = "items head">
+                            <p>OMLET</p>
+                            <hr></hr>
+                        </div>
+                        <div class = "items price">
+                            <p class="old">Rp28.000</p>
+                            <p class="new">Rp19.900</p>
+                        </div>
+                        <div class="items cart">
+                            <i class="fa fa-shopping-cart"></i>
+                            <span>BUY NOW</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3 ">
+                <div class="card-product card-product--menu-cheese-cake">
+                    <div class="overlay">
+                        <div class = "items"></div>
+                        <div class = "items head">
+                            <p>CHEESE CAKE</p>
+                            <hr></hr>
+                        </div>
+                        <div class = "items price">
+                            <p class="old">Rp29.000</p>
+                            <p class="new">Rp20.000</p>
+                        </div>
+                        <div class="items cart">
+                            <i class="fa fa-shopping-cart"></i>
+                            <span>BUY NOW</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3 ">
+                <div class="card-product card-product--menu-manggo-puding">
+                    <div class="overlay">
+                        <div class = "items"></div>
+                        <div class = "items head">
+                            <p>MANGGO PUDING</p>
+                            <hr></hr>
+                        </div>
+                        <div class = "items price">
+                            <p class="old">Rp29.000</p>
+                            <p class="new">Rp27.000</p>
+                        </div>
+                        <div class="items cart">
+                            <i class="fa fa-shopping-cart"></i>
+                            <span>BUY NOW</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+
   );
 }
 
