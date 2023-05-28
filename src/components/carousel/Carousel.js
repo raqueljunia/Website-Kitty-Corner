ce89b72d32a424b6d4f879071d1d35c14531acdd
yohanandre <yohanandrey14@gmail.com>
2023-05-29 05:01:28 +0700

    update

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -1,31 +1,38 @@
-import react from 'react';
+import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
-import carouselItem1 from '../../assets/img/bgSalon1.png'
-import carouselItem2 from '../../assets/img/catGroom1.png'
-
+import carouselItem1 from '../../assets/img/bgSalon1.png';
+import carouselItem2 from '../../assets/img/catGroom1.png';
 
 const CarouselComponent = () => {
-    return(
-        <div>
-        <Carousel>
+  const carouselImageStyle = {
+    width: '100%',
+    height: 'auto',
+    objectFit: 'cover',
+    maxHeight: '700px', // Sesuaikan tinggi maksimal yang diinginkan
+  };
+
+  return (
+    <div>
+      <Carousel>
         <Carousel.Item interval={3000}>
           <img
             className="d-block w-100"
             src={carouselItem1}
             alt="First slide"
+            style={carouselImageStyle}
           />
-         
         </Carousel.Item>
         <Carousel.Item interval={3000}>
           <img
             className="d-block w-100"
             src={carouselItem2}
             alt="Second slide"
+            style={carouselImageStyle}
           />
         </Carousel.Item>
       </Carousel>
-        </div>
-    )
-}
+    </div>
+  );
+};
 
 export default CarouselComponent;
