65c6c34cc912684c0f49ac57e01faf555ba416b3
rifqahaulia <129581643+rifqahaulia@users.noreply.github.com>
2023-05-28 21:26:50 +0700

    Update NavbarComponent.js

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -50,7 +50,7 @@ const NavbarComponent = () => {
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mx-auto">
+          <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
             <NavDropdown title="Services" id="basic-nav-dropdown">
               <NavDropdown.Item href="/services/catSalon">Cat Salon</NavDropdown.Item>
